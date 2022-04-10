2244f235bb88ed608b2d0e542dfd900ec38e9106
Chinemerem Jideama <ncjideama1@gmail.com>
2022-04-10 05:46:45 +0100

    database connection and model creation

---
+++ config/Database.php
@@ -0,0 +1,26 @@
+<?php
+    class Database {
+        //DB PARAMS
+        private $host = 'localhost';
+        private $db_name = 'myblog';
+        private $db_user = 'root';
+        private $db_password = '';
+        private $conn;
+
+
+        //DB CONNECT
+        public function connect() {
+            $this->conn = null;
+
+            try {
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+                $this->conn->setATTRIBUTE(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }catch(PDOException $e){
+                echo 'Connection Error:'. $e->getMessage();
+            }
+
+            return $this->conn;
+        }
+    }
+
+?>
